4cf42f6e035960e67dd2edc03fc9a9ca6016d83d
ismail mert kaynar <160685516+fleaxel@users.noreply.github.com>
2025-07-06 22:17:09 +0200

    Create Subject.java
    Added Subject entity with JPA annotations
    
    done by ismail mert kaynar

---
+++ src/main/java/pl/wwsis/sss/model/Subject.java
@@ -0,0 +1,55 @@
+package pl.wwsis.sss.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subjects")
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "lecturer")
+    private String lecturer;
+
+    // --- Getters and Setters ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+}
